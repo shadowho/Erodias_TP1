8b222416f53d6719e15a990de22d778ea3d18d20
Abengasim <erodias.130308@caloocan.sti.edu.ph>
2023-03-08 21:31:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/erodias_tp1/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.erodias_tp1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
